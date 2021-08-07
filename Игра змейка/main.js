6bf4d148626eac54821b4170aa4f3c182825729b
study657 <79162199605@list.ru>
2021-08-07 03:52:12 +0300

    Игра змейка. Сделана проверка на то скушала ли змейка еду или нет и если скушала, то еда удаляется и потом создается новая. Так же активировали счетчики, когда змейка кушает

--- Игра змейка/main.js
+++ Игра змейка/main.js
@@ -2,19 +2,22 @@ let scoreBlock = document.querySelector('.scoreBlock'),
     bestScoreBlock = document.querySelector('.bestScoreBlock'),
     timerBlock = document.querySelector('.timerBlock'),
     gameBlock = document.querySelector('.gameBlock'),
-    snakeBlock = document.querySelector('.snake');
-head_snake = document.querySelector('.head_snake'),
+    snakeBlock = document.querySelector('.snake'),
+    head_snake = document.querySelector('.head_snake'),
     buttonStart = document.querySelector('.buttonStart'); // Получение элементов верстки
 
 let allCoordinates_top = ['0', '20px', '40px', '60px', '80px', '120px', '140px', '160px', '180px', '200px', '220px', '240px', '260px', '280px', '300px', '320px', '340px', '360px', '380px', '400px', '420px', '440px', '460px', '480px'];
 let allCoordinates_left = ['0', '20px', '40px', '60px', '80px', '120px', '140px', '160px', '180px', '200px', '220px', '240px', '260px', '280px', '300px', '320px', '340px', '360px', '380px', '400px', '420px', '440px', '460px', '480px', '500px', '520px', '540px', '560px', '580px', '600px', '620px', '640px', '660px', '680px', '700px', '720px', '740px'];
 let currentOrientation = 'rotate(180deg)'; // Текущая ориентация поворота головы змеи
 let currentDirecrion = ''; // Текущее направление, куда смотрит змейка (право, вниз, лево, вверх)
+let valueSpeedSnake = 100; // Текущая скорость змейки
 let timeCurr = 0; // Текущий показатель времени в секундах
-let valueSpeedSnake = 1000; // Текущая скорость змейки
+let scoreCurr = 0; // Текущий показатель счета
+let bestScoreCurr = 0; // Текущий показатель лучшего счета
 
 let currentCoordinatesLeft = []; // Координата x головы змейки (всегда положительное значение)
 let currentCoordinatesTop = []; // Координата y головы змейки (всегда положительное значение)
+let paramsFoodSnake;
 
 
 window.addEventListener('keydown', function (event) { // Вещаем обработчик события на весь экран и отслеживаем нажатие клавиш на нем, для управления змейки и в случае нажатия клавиши, меняем текущее направление
@@ -39,7 +42,9 @@ window.addEventListener('keydown', function (event) { // Вещаем обраб
 
 buttonStart.addEventListener('click', function () { // Повесили обработчик события на кнопку "начать игру"
     buttonStart.disabled = 'true'; // Сделали кнопочку не активной и не кликабельной
-    let paramsFoodSnake = createSnakeFood(); // Вызываем функцию, которая заспавнит еду для змейки, а так же вернут нам массив с информацией о этой еде
+    if (paramsFoodSnake == undefined) { // Вызываем функцию, которая заспавнит еду для змейки, а так же вернут нам массив с информацией о этой еде
+        paramsFoodSnake = createSnakeFood();
+    }
 
     let timerTime = setInterval(function () { // Запустили таймер времени в секундах
         timerBlock.innerHTML = 'Время: ' + timeCurr + ' сек';
@@ -47,10 +52,11 @@ buttonStart.addEventListener('click', function () { // Повесили обра
     }, 1000);
 
     let speedSnake = setInterval(function () { // Запустили движение змейки по скорости, определенной в переменной
-        paramsFoodSnake = createSnakeFood();  // Вызываем функцию, которая заспавнит еду для змейки, а так же вернут нам массив с информацией о этой еде
         if (currentDirecrion == '') { // Если пока что не присвоена никакая ориентация, т.к. клавиши еще не нажаты, то змейка движется слева направо
             let bodySnakes = document.querySelectorAll('.body_snake');
             let currentCoordinatesHeadSnake_left = getComputedStyle(head_snake).left;
+            let currentCoordinatesHeadSnake_top = getComputedStyle(head_snake).top;
+            chekingSnakeEatFoodAndExpansionSnake(currentCoordinatesHeadSnake_top, currentCoordinatesHeadSnake_left, paramsFoodSnake['Y'], paramsFoodSnake['X'], paramsFoodSnake['elem'], bodySnakes);
             if (currentOrientation == 'rotate(180deg)') {
                 currentCoordinatesLeft.push(currentCoordinatesHeadSnake_left);
 
@@ -76,6 +82,7 @@ buttonStart.addEventListener('click', function () { // Повесили обра
             let bodySnakes = document.querySelectorAll('.body_snake');
             let currentCoordinatesHeadSnake_left = getComputedStyle(head_snake).left;
             let currentCoordinatesHeadSnake_top = getComputedStyle(head_snake).top;
+            chekingSnakeEatFoodAndExpansionSnake(currentCoordinatesHeadSnake_top, currentCoordinatesHeadSnake_left, paramsFoodSnake['Y'], paramsFoodSnake['X'], paramsFoodSnake['elem'], bodySnakes);
             if (currentOrientation == 'rotate(180deg)') {
                 currentCoordinatesLeft.push(currentCoordinatesHeadSnake_left);
                 currentCoordinatesTop.push(currentCoordinatesHeadSnake_top);
@@ -106,6 +113,7 @@ buttonStart.addEventListener('click', function () { // Повесили обра
             let bodySnakes = document.querySelectorAll('.body_snake');
             let currentCoordinatesHeadSnake_left = getComputedStyle(head_snake).left;
             let currentCoordinatesHeadSnake_top = getComputedStyle(head_snake).top;
+            chekingSnakeEatFoodAndExpansionSnake(currentCoordinatesHeadSnake_top, currentCoordinatesHeadSnake_left, paramsFoodSnake['Y'], paramsFoodSnake['X'], paramsFoodSnake['elem'], bodySnakes);
             if (currentOrientation == 'rotate(270deg)') {
                 currentCoordinatesLeft.push(currentCoordinatesHeadSnake_left);
                 currentCoordinatesTop.push(currentCoordinatesHeadSnake_top);
@@ -136,6 +144,7 @@ buttonStart.addEventListener('click', function () { // Повесили обра
             let bodySnakes = document.querySelectorAll('.body_snake');
             let currentCoordinatesHeadSnake_left = getComputedStyle(head_snake).left;
             let currentCoordinatesHeadSnake_top = getComputedStyle(head_snake).top;
+            chekingSnakeEatFoodAndExpansionSnake(currentCoordinatesHeadSnake_top, currentCoordinatesHeadSnake_left, paramsFoodSnake['Y'], paramsFoodSnake['X'], paramsFoodSnake['elem'], bodySnakes);
             if (currentOrientation == 'rotate(0deg)') {
                 currentCoordinatesLeft.push(currentCoordinatesHeadSnake_left);
                 currentCoordinatesTop.push(currentCoordinatesHeadSnake_top);
@@ -166,6 +175,7 @@ buttonStart.addEventListener('click', function () { // Повесили обра
             let bodySnakes = document.querySelectorAll('.body_snake');
             let currentCoordinatesHeadSnake_left = getComputedStyle(head_snake).left;
             let currentCoordinatesHeadSnake_top = getComputedStyle(head_snake).top;
+            chekingSnakeEatFoodAndExpansionSnake(currentCoordinatesHeadSnake_top, currentCoordinatesHeadSnake_left, paramsFoodSnake['Y'], paramsFoodSnake['X'], paramsFoodSnake['elem'], bodySnakes);
             if (currentOrientation == 'rotate(90deg)') {
                 currentCoordinatesLeft.push(currentCoordinatesHeadSnake_left);
                 currentCoordinatesTop.push(currentCoordinatesHeadSnake_top);
@@ -192,6 +202,10 @@ buttonStart.addEventListener('click', function () { // Повесили обра
                 currentCoordinatesTop.length = 0;
             }
         }
+
+        if (paramsFoodSnake == undefined) { // Вызываем функцию, которая заспавнит еду для змейки, а так же вернут нам массив с информацией о этой еде
+            paramsFoodSnake = createSnakeFood();
+        }
     }, valueSpeedSnake);
 });
 
@@ -214,40 +228,62 @@ function createSnakeFood() { // Функция, которая создает и
             coordinatesLeftFromCurrentSnake.push(getComputedStyle(body_snakes[i]).left);
         }
 
-        let randomCoordinateY = allCoordinates_top[getRandomIntInclusive(0, allCoordinates_top.length - 1)],
-            randomCoordinateX = allCoordinates_left[getRandomIntInclusive(0, allCoordinates_left.length - 1)]; // Рандомно получаем координаты по X и Y
+        while (true) {
+            let randomCoordinateY = parseInt(allCoordinates_top[getRandomIntInclusive(0, allCoordinates_top.length - 1)]) + 'px',
+                randomCoordinateX = parseInt(allCoordinates_left[getRandomIntInclusive(0, allCoordinates_left.length - 1)]) + 'px'; // Рандомно получаем координаты по X и Y
 
-        for (let i = 0; i < coordinatesTopFromCurrentSnake.length; i++) { // Делаем проверку на то, что координаты не повторяются со змейкой
-            if ((coordinatesTopFromCurrentSnake[i] !== randomCoordinateY && coordinatesLeftFromCurrentSnake[i] !== randomCoordinateX) ||
-                (coordinatesTopFromCurrentSnake[i] == randomCoordinateY && coordinatesLeftFromCurrentSnake[i] !== randomCoordinateX) ||
-                (coordinatesLeftFromCurrentSnake[i] == randomCoordinateX && coordinatesTopFromCurrentSnake[i] !== randomCoordinateY)) {
-                chekingArr.push('true');
-            } else {
-                chekingArr.push('false');
+            for (let i = 0; i < coordinatesTopFromCurrentSnake.length; i++) { // Делаем проверку на то, что координаты не повторяются со змейкой
+                if ((coordinatesTopFromCurrentSnake[i] !== randomCoordinateY && coordinatesLeftFromCurrentSnake[i] !== randomCoordinateX) ||
+                    (coordinatesTopFromCurrentSnake[i] == randomCoordinateY && coordinatesLeftFromCurrentSnake[i] !== randomCoordinateX) ||
+                    (coordinatesLeftFromCurrentSnake[i] == randomCoordinateX && coordinatesTopFromCurrentSnake[i] !== randomCoordinateY)) {
+                    chekingArr.push('true');
+                } else {
+                    chekingArr.push('false');
+                }
+            }
+
+            let check = chekingArr.every(function (item) {
+                if (item == 'true') {
+                    return true;
+                } else {
+                    return false;
+                }
+            });
+
+            if (check) { // Если координаты не повторяются со змейкой, тогда создаем змейку по этим координатам, а если повторяются, тогда вызываем функцию заново
+                let foodSnake = createElement('div', snakeBlock, ['snake_food']);
+                foodSnake.style.top = randomCoordinateY;
+                foodSnake.style.left = randomCoordinateX;
+
+                return {
+                    'Y': randomCoordinateY,
+                    'X': randomCoordinateX,
+                    'elem': foodSnake
+                };
             }
         }
+    }
+};
 
-        let check = chekingArr.every(function (item) {
-            if (item == 'true') {
-                return true;
-            } else {
-                return false;
-            }
-        });
 
-        if (check) { // Если координаты не повторяются со змейкой, тогда создаем змейку по этим координатам, а если повторяются, тогда вызываем функцию заново
-            let foodSnake = createElement('div', snakeBlock, ['snake_food']);
-            foodSnake.style.top = randomCoordinateY;
-            foodSnake.style.left = randomCoordinateX;
+function chekingSnakeEatFoodAndExpansionSnake(coordinateTopHeadSnake, coordinateLeftHeadSnake, coordinateTopFood, coordinateLeftFood, elemFood, bodySnakes) { // Функция, которая проверяет скушала ли змейка еду и если да, тогда удаляет старую еду и потом создается новая
+    if (coordinateTopHeadSnake == coordinateTopFood && coordinateLeftHeadSnake == coordinateLeftFood) {
+        elemFood.remove();
+        paramsFoodSnake = undefined;
 
-            return {
-                'Y': randomCoordinateY,
-                'X': randomCoordinateX,
-                'elem': foodSnake
-            };
-        } else {
-            createSnakeFood();
-        }
+        let lastBodySnake = bodySnakes[bodySnakes.length - 1];
+
+        let x = getComputedStyle(lastBodySnake).left;
+        let y = getComputedStyle(lastBodySnake).top;
+
+        let elem = createElement('div', snakeBlock, ['body_snake']);
+        elem.style.left = x;
+        elem.style.top = y;
+
+        scoreCurr++;
+        bestScoreCurr++;
+        scoreBlock.innerHTML = 'Счет: ' + scoreCurr;
+        bestScoreBlock.innerHTML = 'Лучший счет: ' + scoreCurr;
     }
 };
 
