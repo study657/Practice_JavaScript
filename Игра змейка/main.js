ba8edeebe39324daebe70c37acc6cdb37e2a4ab4
study657 <79162199605@list.ru>
2021-08-13 01:19:12 +0300

    Игра змейка. Логика игры полностью готова. А именно сделано так, что змейка двигается и ей можно управлять через клавиатуру, рандомно спавнится еда, там где нет змейки. Эту еду змейка может кушать и если она ее съела, то она появляется в новом месте. Если змейка ударяется головой о себя же, то тогда игра оканчивается и с помощью новой кнопочки ее можно рестартануть в полной мере, так же добавлено сохранение в локальное хранилище лучшего счета игрока и после рестарта страницы оно не обнуляется. Исправлена проблема с зависанием игры, подробнее опишу решение ошибки в комментарии к этому коммиту

--- Игра змейка/main.js
+++ Игра змейка/main.js
@@ -5,24 +5,28 @@ let parentBlock = document.querySelector('.col-md-8'),
     gameBlock = document.querySelector('.gameBlock'),
     snakeBlock = document.querySelector('.snake'),
     head_snake = document.querySelector('.head_snake'),
+    snake_food = document.querySelector('.snake_food'),
     buttonStart = document.querySelector('.buttonStart'); // Получение элементов верстки
 
 let allCoordinates_top = ['0', '20px', '40px', '60px', '80px', '120px', '140px', '160px', '180px', '200px', '220px', '240px', '260px', '280px', '300px', '320px', '340px', '360px', '380px', '400px', '420px', '440px', '460px', '480px']; // Все координаты по оси Y
 let allCoordinates_left = ['0', '20px', '40px', '60px', '80px', '120px', '140px', '160px', '180px', '200px', '220px', '240px', '260px', '280px', '300px', '320px', '340px', '360px', '380px', '400px', '420px', '440px', '460px', '480px', '500px', '520px', '540px', '560px', '580px', '600px', '620px', '640px', '660px', '680px', '700px', '720px', '740px']; // Все координаты по оси X
 let currentOrientation = 'rotate(180deg)'; // Текущая ориентация поворота головы змеи
 let currentDirecrion = ''; // Текущее направление, куда смотрит змейка (право, вниз, лево, вверх)
-let valueSpeedSnake = 100; // Текущая скорость змейки
 let timeCurr = 0; // Текущий показатель времени в секундах
 let scoreCurr = 0; // Текущий показатель счета
 let bestScoreCurr = 0; // Текущий показатель лучшего счета
 
 let currentCoordinatesLeft = []; // Координата x головы змейки (всегда положительное значение)
 let currentCoordinatesTop = []; // Координата y головы змейки (всегда положительное значение)
-let paramsFoodSnake; // Параметры еды, которые создаются в момент создания пищи для змейки
+let paramsFoodSnake = { // Параметры еды, которые создаются в момент создания пищи для змейки
+    'X': '400px',
+    'Y': '80px',
+    'elem': snake_food
+}
 
 if (localStorage.getItem('bestScore') !== null) { // Проверка на локальное хранилище с Лучшим счетом игрока
     bestScoreBlock.innerHTML = bestScoreBlock.innerHTML + localStorage.getItem('bestScore');
-}else{
+} else {
     bestScoreBlock.innerHTML = 'Лучший счет: 0';
 }
 
@@ -49,20 +53,22 @@ window.addEventListener('keydown', function (event) { // Вещаем обраб
 
 buttonStart.addEventListener('click', function () { // Повесили обработчик события на кнопку "начать игру"
     buttonStart.disabled = 'true'; // Сделали кнопочку не активной и не кликабельной
-    if (paramsFoodSnake == undefined) { // Вызываем функцию, которая заспавнит еду для змейки, а так же вернут нам массив с информацией о этой еде
-        paramsFoodSnake = createSnakeFood();
-    }
 
     let timerTime = setInterval(function () { // Запустили таймер времени в секундах
         timerBlock.innerHTML = 'Время: ' + timeCurr + ' сек';
         timeCurr++;
     }, 1000);
 
-    let speedSnake = setInterval(function () { // Запустили движение змейки по скорости, определенной в переменной
+    let speedSnake = setInterval(function () { // Запустили движение змейки по скорости
+        let bodySnakes = document.querySelectorAll('.body_snake');
+        let currentCoordinatesHeadSnake_left = getComputedStyle(head_snake).left;
+        let currentCoordinatesHeadSnake_top = getComputedStyle(head_snake).top;
+
+        if (paramsFoodSnake == undefined) { // Вызываем функцию, которая заспавнит еду для змейки, а так же вернет нам объект с информацией о этой еде
+            paramsFoodSnake = createSnakeFood(bodySnakes);
+        }
+
         if (currentDirecrion == '') { // Если пока что не присвоена никакая ориентация, т.к. клавиши еще не нажаты, то змейка движется слева направо
-            let bodySnakes = document.querySelectorAll('.body_snake');
-            let currentCoordinatesHeadSnake_left = getComputedStyle(head_snake).left;
-            let currentCoordinatesHeadSnake_top = getComputedStyle(head_snake).top;
             chekingSnakeEatFoodAndExpansionSnake(currentCoordinatesHeadSnake_top, currentCoordinatesHeadSnake_left, paramsFoodSnake['Y'], paramsFoodSnake['X'], paramsFoodSnake['elem'], bodySnakes); // Проверка на то скушала ли змейка еду
             if (currentOrientation == 'rotate(180deg)') {
                 currentCoordinatesLeft.push(currentCoordinatesHeadSnake_left);
@@ -86,9 +92,6 @@ buttonStart.addEventListener('click', function () { // Повесили обра
             }
         } else if (currentDirecrion == 'Право') { // Змейка движется направо
             currentOrientation = 'rotate(180deg)';
-            let bodySnakes = document.querySelectorAll('.body_snake');
-            let currentCoordinatesHeadSnake_left = getComputedStyle(head_snake).left;
-            let currentCoordinatesHeadSnake_top = getComputedStyle(head_snake).top;
             chekingSnakeEatFoodAndExpansionSnake(currentCoordinatesHeadSnake_top, currentCoordinatesHeadSnake_left, paramsFoodSnake['Y'], paramsFoodSnake['X'], paramsFoodSnake['elem'], bodySnakes);
             if (currentOrientation == 'rotate(180deg)') {
                 currentCoordinatesLeft.push(currentCoordinatesHeadSnake_left);
@@ -117,9 +120,6 @@ buttonStart.addEventListener('click', function () { // Повесили обра
             }
         } else if (currentDirecrion == 'Вниз') { // Змейка движется вниз
             currentOrientation = 'rotate(270deg)';
-            let bodySnakes = document.querySelectorAll('.body_snake');
-            let currentCoordinatesHeadSnake_left = getComputedStyle(head_snake).left;
-            let currentCoordinatesHeadSnake_top = getComputedStyle(head_snake).top;
             chekingSnakeEatFoodAndExpansionSnake(currentCoordinatesHeadSnake_top, currentCoordinatesHeadSnake_left, paramsFoodSnake['Y'], paramsFoodSnake['X'], paramsFoodSnake['elem'], bodySnakes);
             if (currentOrientation == 'rotate(270deg)') {
                 currentCoordinatesLeft.push(currentCoordinatesHeadSnake_left);
@@ -148,9 +148,6 @@ buttonStart.addEventListener('click', function () { // Повесили обра
             }
         } else if (currentDirecrion == 'Лево') { // Змейка движется влево
             currentOrientation = 'rotate(0deg)';
-            let bodySnakes = document.querySelectorAll('.body_snake');
-            let currentCoordinatesHeadSnake_left = getComputedStyle(head_snake).left;
-            let currentCoordinatesHeadSnake_top = getComputedStyle(head_snake).top;
             chekingSnakeEatFoodAndExpansionSnake(currentCoordinatesHeadSnake_top, currentCoordinatesHeadSnake_left, paramsFoodSnake['Y'], paramsFoodSnake['X'], paramsFoodSnake['elem'], bodySnakes);
             if (currentOrientation == 'rotate(0deg)') {
                 currentCoordinatesLeft.push(currentCoordinatesHeadSnake_left);
@@ -179,9 +176,6 @@ buttonStart.addEventListener('click', function () { // Повесили обра
             }
         } else if (currentDirecrion == 'Вверх') { // Змейка движется вверх
             currentOrientation = 'rotate(90deg)';
-            let bodySnakes = document.querySelectorAll('.body_snake');
-            let currentCoordinatesHeadSnake_left = getComputedStyle(head_snake).left;
-            let currentCoordinatesHeadSnake_top = getComputedStyle(head_snake).top;
             chekingSnakeEatFoodAndExpansionSnake(currentCoordinatesHeadSnake_top, currentCoordinatesHeadSnake_left, paramsFoodSnake['Y'], paramsFoodSnake['X'], paramsFoodSnake['elem'], bodySnakes);
             if (currentOrientation == 'rotate(90deg)') {
                 currentCoordinatesLeft.push(currentCoordinatesHeadSnake_left);
@@ -209,63 +203,48 @@ buttonStart.addEventListener('click', function () { // Повесили обра
                 currentCoordinatesTop.length = 0;
             }
         }
-
-        if (paramsFoodSnake == undefined) { // Вызываем функцию, которая заспавнит еду для змейки, а так же вернут нам массив с информацией о этой еде
-            paramsFoodSnake = createSnakeFood();
-        }
         checkLoseAndRestartGame(timerTime, speedSnake); // Проверка на то, не врезалась ли змейка сама в себя и не окончилась ли игра из-за этого
-    }, valueSpeedSnake);
+    }, 100);
 });
 
-
-function createSnakeFood() { // Функция, которая создает и добавляет визуально еду для змейки
+function createSnakeFood(body_snakes) { // Функция, которая создает и добавляет визуально еду для змейки
     let snakeFoodElem = document.querySelector('.snake_food');
-    let coordinatesTopFromCurrentSnake = [];
-    let coordinatesLeftFromCurrentSnake = [];
-    let chekingArr = [];
+    let coordinatesXFromCurrentSnake = [];
+    let coordinatesYFromCurrentSnake = [];
 
     if (snakeFoodElem == null) { // Если полученного элемента на странице нет, значит еда для змейки не создана и ее нужно создать
         let head_snake = document.querySelector('.head_snake');
-        let body_snakes = document.querySelectorAll('.body_snake');
 
-        coordinatesTopFromCurrentSnake.push(getComputedStyle(head_snake).top);
-        coordinatesLeftFromCurrentSnake.push(getComputedStyle(head_snake).left);
+        coordinatesXFromCurrentSnake.push(getComputedStyle(head_snake).left);
+        coordinatesYFromCurrentSnake.push(getComputedStyle(head_snake).top);
 
         for (let i = 0; i < body_snakes.length; i++) { // Заполняем массивы с координатами, которые сейчас имеет змейка
-            coordinatesTopFromCurrentSnake.push(getComputedStyle(body_snakes[i]).top);
-            coordinatesLeftFromCurrentSnake.push(getComputedStyle(body_snakes[i]).left);
+            coordinatesXFromCurrentSnake.push(getComputedStyle(body_snakes[i]).left);
+            coordinatesYFromCurrentSnake.push(getComputedStyle(body_snakes[i]).top);
         }
 
-        while (true) {
-            let randomCoordinateY = parseInt(allCoordinates_top[getRandomIntInclusive(0, allCoordinates_top.length - 1)]) + 'px',
-                randomCoordinateX = parseInt(allCoordinates_left[getRandomIntInclusive(0, allCoordinates_left.length - 1)]) + 'px'; // Рандомно получаем координаты по X и Y
+        while (true) { // Запускается бесконечный цикл, который генерирует рандомно координаты еды по оси X и Y, после чего идет проверка на то, пересекаются ли эти координаты с одной из тел змейки и если да, то запускается все по новой, а если нет, то цикл останавливается и нам выдают объект с нужными параметрами
+            let randomCoordinateX = parseInt(allCoordinates_left[getRandomIntInclusive(0, allCoordinates_left.length - 1)]) + 'px', // Рандомно получаем координаты по X и Y
+                randomCoordinateY = parseInt(allCoordinates_top[getRandomIntInclusive(0, allCoordinates_top.length - 1)]) + 'px';
 
-            for (let i = 0; i < coordinatesTopFromCurrentSnake.length; i++) { // Делаем проверку на то, что координаты не повторяются со змейкой
-                if ((coordinatesTopFromCurrentSnake[i] !== randomCoordinateY && coordinatesLeftFromCurrentSnake[i] !== randomCoordinateX) ||
-                    (coordinatesTopFromCurrentSnake[i] == randomCoordinateY && coordinatesLeftFromCurrentSnake[i] !== randomCoordinateX) ||
-                    (coordinatesLeftFromCurrentSnake[i] == randomCoordinateX && coordinatesTopFromCurrentSnake[i] !== randomCoordinateY)) {
-                    chekingArr.push('true');
-                } else {
-                    chekingArr.push('false');
-                }
-            }
-
-            let check = chekingArr.every(function (item) {
-                if (item == 'true') {
+            let check = coordinatesXFromCurrentSnake.every(function (elem, index) {
+                if ((coordinatesYFromCurrentSnake[index] !== randomCoordinateY && coordinatesXFromCurrentSnake[index] !== randomCoordinateX) ||
+                    (coordinatesYFromCurrentSnake[index] == randomCoordinateY && coordinatesXFromCurrentSnake[index] !== randomCoordinateX) ||
+                    (coordinatesXFromCurrentSnake[index] == randomCoordinateX && coordinatesYFromCurrentSnake[index] !== randomCoordinateY)) {
                     return true;
                 } else {
                     return false;
                 }
             });
 
-            if (check) { // Если координаты не повторяются со змейкой, тогда создаем змейку по этим координатам, а если повторяются, тогда вызываем функцию заново
+            if (check) {
                 let foodSnake = createElement('div', snakeBlock, ['snake_food']);
                 foodSnake.style.top = randomCoordinateY;
                 foodSnake.style.left = randomCoordinateX;
 
                 return {
-                    'Y': randomCoordinateY,
                     'X': randomCoordinateX,
+                    'Y': randomCoordinateY,
                     'elem': foodSnake
                 };
             }
@@ -318,7 +297,7 @@ function checkLoseAndRestartGame(timerId1, timerId2) { // Функция, кот
 
             if (localStorage.getItem('bestScore') == null) { // Добавление в локальное хранилище лучшего счета игрока
                 localStorage.setItem('bestScore', bestScoreCurr);
-            } else if(Number(localStorage.getItem('bestScore')) < bestScoreCurr){
+            } else if (Number(localStorage.getItem('bestScore')) < bestScoreCurr) {
                 localStorage.setItem('bestScore', bestScoreCurr);
             }
 
@@ -330,7 +309,12 @@ function checkLoseAndRestartGame(timerId1, timerId2) { // Функция, кот
 
                 if (paramsFoodSnake['elem'] !== undefined) {
                     paramsFoodSnake['elem'].remove();
-                    paramsFoodSnake = undefined;
+                    let newFoodSnake = createElement('div', snakeBlock, ['snake_food']);
+                    paramsFoodSnake = {
+                        'X': '400px',
+                        'Y': '80px',
+                        'elem': newFoodSnake
+                    };
                     buttonStart.removeAttribute('disabled');
 
                     let bodySnakes = document.querySelectorAll('.body_snake');
