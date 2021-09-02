fa1d168bb61cdcc82c265b05f4ee735349133966
study657 <79162199605@list.ru>
2021-09-02 20:22:31 +0300

    Игра дурак. Пытаемся реализовать логику побития карты игрока, после его хода

--- Игра дурак/main.js
+++ Игра дурак/main.js
@@ -64,7 +64,19 @@ if (startMovie) {
             cardsPlayer.splice(index, 1);
             console.log(cardsPlayer, index);
 
-            checkAbilityBeatPlayerCard(cardsComputer, this);
+            let cardForBeat = checkAbilityBeatPlayerCard(cardsComputer, this);
+
+            if (cardForBeat && !this.classList.contains(trumpCard)) {
+                console.log(cardForBeat);
+            } else {
+                if(this.classList.contains(trumpCard)){
+                    cardForBeat = checkAbilityBeatPlayerCard(cardsComputer, this);
+                    console.log('Это козырь и мы бьем его козырем!', cardForBeat);
+                }else{
+                    cardForBeat = getKozirForBeatPlayerCard(cardsComputer);
+                    console.log('Это обычная карта, но ее нет у игрока и мы бьем козырем!', cardForBeat);
+                }
+            }
         });
     }
 } else {
@@ -255,18 +267,43 @@ function sortCardsForPlayer(cardsArr) { // Функция, которая сор
     }
 };
 
-function checkAbilityBeatPlayerCard(cardsComputer, card) { // Функция, которая делает проверку на то, способен ли компьютер побить карту игрока
+function checkAbilityBeatPlayerCard(cardsComputer, card) { // Функция, которая делает проверку на то, способен ли компьютер побить обычную карту игрока, т.е. проверка на данную масть и если такая масть имеется, которая выше, чем у игрока, тогда мы возвращаем этот элемент
     let mastCard = card.classList[0];
     let weigthCard = getweigthCardRegardingMasti(card, trumpCard, 2);
-    console.log(mastCard, weigthCard);
 
+    // ПРОБЛЕМА В ТОМ, ЧТО МЫ ПОЛУЧАЕМ НЕ САМУЮ МАЛЕНЬКУЮ КАРТУ ДЛЯ ТОГО, ЧТОБЫ ЕЕ ПОБИТЬ. Т.Е. К ПРИМЕРУ ИГРОК ХОДИТ ШЕСТЕРКОЙ КРЕСТИ, У КОМПЬЮТЕРА ЕСТЬ 8 КРЕСТИ И 10 КРЕСТИ, ТАК ВОТ ОН МОЖЕТ ПОЙТИ 10 КРЕСТИ, ХОТЯ ЦЕЛЕСООБРАЗНО ПОЙТИ 8 КРЕСТИ!
+    // ??????????????????????????????????????????????????????????????????????????????
     for (let i = 0; i < cardsComputer.length; i++) {
         if (cardsComputer[i].classList.contains(mastCard)) {
-            if(getweigthCardRegardingMasti(cardsComputer[i], trumpCard, 1) > weigthCard){
-                console.log('МОГУ ПОБИТЬ ЭТУ КАРТУ НА ИЗИ!', cardsComputer[i]);
-                break;
+            if (getweigthCardRegardingMasti(cardsComputer[i], trumpCard, 1) > weigthCard) {
+                return cardsComputer[i];
             }
         }
+    }
+    return false;
+};
+
+function getKozirForBeatPlayerCard(cardsComputer) { // Функция, которая отдает нам карту, которая козырная и происходит это в том случае, если у компьютер не может побить карту без козыря и у него просто нет подходящей карты
+    let kozirsComputerArr = [];
+    let weigthComputerArr = [];
+
+    for (let i = 0; i < cardsComputer.length; i++) {
+        if (cardsComputer[i].classList.contains(trumpCard)) {
+            kozirsComputerArr.push(cardsComputer[i]);
+        }
+    }
+
+    if (kozirsComputerArr.length == 0) {
+        return false;
+    } else {
+        for (let i = 0; i < kozirsComputerArr.length; i++) {
+            weigthComputerArr.push(getweigthCardRegardingMasti(kozirsComputerArr[i], trumpCard, 1));
+        }
+
+        let minWeigthCard = Math.min(...weigthComputerArr);
+        let index = weigthComputerArr.indexOf(minWeigthCard);
+
+        return kozirsComputerArr[index];
     }
 };
 
